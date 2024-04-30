7e95a4b09c1daabfcbb033e5b75c09ebfc4e53e4
kyawThetHtun <kyawthe235@gmail.com>
2024-04-30 21:54:24 +0630

    versioning

--- nasa_project/server/src/app.js
+++ nasa_project/server/src/app.js
@@ -3,8 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan')
 
-const planetRouter = require('./routes/planets/planets.router')
-const launchesRouter = require('./routes/launches/launches.router')
+const api = require('./routes/api');
 
 const app = express();
 
@@ -12,13 +11,13 @@ app.use(cors({
   origin : 'http://localhost:3000'
 }));
 app.use(morgan('tiny'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'..','public', 'build')))
 
-app.use(planetRouter);
-app.use('/launches',launchesRouter);
+app.use('/v1',api);
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'public', 'build' , 'index.html'));
 });
 
